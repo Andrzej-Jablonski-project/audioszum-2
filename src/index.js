9139ab4fe8ede5286c60c86d72896568c6887439
Andrzej Jabłoński <andrzej@audioszum.pl>
2021-02-02 23:16:14 +0100

    Fixed links in the header

--- src/index.js
+++ src/index.js
@@ -36,57 +36,60 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
 })
 
-const data = 'https://digi-chip.pl/wp-json/wp/v2/posts?';
+const card = document.querySelector('.blog-list');
 
-fetch(data, {
-  method: 'GET',
-})
-  .then(response => response.json())
-  .then(response => {
+if (card) {
 
-    const card = document.querySelector('.blog-list');
+  const data = 'https://digi-chip.pl/wp-json/wp/v2/posts?';
 
-    function createCard(link, categories, title, image) {
+  fetch(data, {
+    method: 'GET',
+  })
+    .then(response => response.json())
+    .then(response => {
 
-      const category = {
-        153: 'Audio-Video',
-        154: 'Komputery',
-        155: 'Oprogramowanie',
-        158: 'Programowanie',
-        160: 'Projekty',
+      function createCard(link, categories, title, image) {
 
-        isNumber() {
-          const [value] = categories
-          return value
+        const category = {
+          153: 'Audio-Video',
+          154: 'Komputery',
+          155: 'Oprogramowanie',
+          158: 'Programowanie',
+          160: 'Projekty',
+
+          isNumber() {
+            const [value] = categories
+            return value
+          }
         }
-      }
 
-      card.innerHTML += `<li class= "w-full mb-4 text-lg bg-mercury sm:rounded-lg overflow-hidden shadow hover:shadow-lg transition duration-150 ease-in-out" >
+        card.innerHTML += `<li class= "w-full mb-4 text-lg bg-mercury sm:rounded-lg overflow-hidden shadow hover:shadow-lg transition duration-150 ease-in-out" >
                 <a href="${link}"><img class="m-auto object-cover w-full h-48" src="${image}" loadin="lazy" alt=""></a><h3 class="p-6 text-sm text-astral font-semibold tracking-tight">${category[category.isNumber()]}</h3 >
                 <a class="hover:underline" href="${link}"><p class="px-6 pb-6 text-xl font-extrabold text-fiord">${title}</p></a></li > `;
-    }
+      }
 
-    for (let posts of response) {
+      for (let posts of response) {
 
-      const { link, title, categories, images } = posts;
+        const { link, title, categories, images } = posts;
 
-      createCard(link, categories, title.rendered, images.large);
-    }
+        createCard(link, categories, title.rendered, images.large);
+      }
 
-    function countCard(numberOfCard) {
+      function countCard(numberOfCard) {
 
-      const cards = document.querySelectorAll('.blog-list>li');
+        const cards = document.querySelectorAll('.blog-list>li');
 
-      for (let i = 0; i < cards.length; i++) {
-        if (i > numberOfCard - 1) {
-          cards[i].classList.add('hidden');
+        for (let i = 0; i < cards.length; i++) {
+          if (i > numberOfCard - 1) {
+            cards[i].classList.add('hidden');
+          }
         }
       }
-    }
-    countCard(6);
-  })
+      countCard(6);
+    })
 
-  .catch(err => {
-    console.log('Nie udało się pobrać danych');
-    console.log(err)
-  })
+    .catch(err => {
+      console.log('Nie udało się pobrać danych');
+      console.log(err)
+    })
+}
