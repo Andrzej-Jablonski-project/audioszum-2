ffbf85c58812ce52b943921b9232460e9c4778f8
Andrzej Jabłoński <andrzej@audioszum.pl>
2021-04-30 15:13:58 +0200

    fixes the download of the DOM element

--- src/index.js
+++ src/index.js
@@ -13,30 +13,28 @@ document.addEventListener("DOMContentLoaded", loadScripts);
 
 function loadScripts() {
     toggleMobileMenu.bind(mobileMenu);
-    const loadAnimation = sectionBlog.querySelector(".sk-wave");
 
-    window.addEventListener("scroll", displayDataLoadingAnimation);
-
-    function displayDataLoadingAnimation() {
-        const scrollPosition = window.scrollY;
-        const elTopPosition = sectionBlog.offsetTop;
-        if (scrollPosition + 500 > elTopPosition) {
-            loadAnimation.style.display = "flex";
-            setTimeout(() => {
-                loadAnimation.style.display = "none";
-                card.classList.add("box-on");
-                card.classList.remove("box-off");
-                sectionBlog.querySelector(".error-js").classList.add("box-on");
-                sectionBlog
-                    .querySelector(".error-js")
-                    .classList.remove("box-off");
-                window.removeEventListener(
-                    "scroll",
-                    displayDataLoadingAnimation,
-                );
-            }, 3000);
-        }
+    if (sectionBlog) {
+        window.addEventListener("scroll", displayDataLoadingAnimation);
     }
+
     buildBlogCards();
     sendForm();
+}
+
+function displayDataLoadingAnimation() {
+    const loadAnimation = sectionBlog.querySelector(".sk-wave");
+    const scrollPosition = window.scrollY;
+    const elTopPosition = sectionBlog.offsetTop;
+    if (scrollPosition + 500 > elTopPosition) {
+        loadAnimation.style.display = "flex";
+        setTimeout(() => {
+            loadAnimation.style.display = "none";
+            card.classList.add("box-on");
+            card.classList.remove("box-off");
+            sectionBlog.querySelector(".error-js").classList.add("box-on");
+            sectionBlog.querySelector(".error-js").classList.remove("box-off");
+            window.removeEventListener("scroll", displayDataLoadingAnimation);
+        }, 3000);
+    }
 }
